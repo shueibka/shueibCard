164d2acb8ca1c307bf083db495361f24084d762d
Shueib Abdirahman <136870666+shueibka@users.noreply.github.com>
2024-01-30 23:02:15 +0100

    done

---
+++ shueibCard/infoView.swift
@@ -0,0 +1,18 @@
+//
+//  infoView.swift
+//  shueibCard
+//
+//  Created by Fahad nur on 2024-01-30.
+//
+
+import SwiftUI
+
+struct infoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    infoView()
+}
