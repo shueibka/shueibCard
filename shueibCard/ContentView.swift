d72e0a9f7a34b4b0616a75bf50e696d2e8c2e536
Shueib Abdirahman <136870666+shueibka@users.noreply.github.com>
2024-01-30 10:54:15 +0100

    Initial Commit

---
+++ shueibCard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  shueibCard
+//
+//  Created by Fahad nur on 2024-01-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ shueibCard/shueibCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  shueibCardApp.swift
+//  shueibCard
+//
+//  Created by Fahad nur on 2024-01-30.
+//
+
+import SwiftUI
+
+@main
+struct shueibCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
